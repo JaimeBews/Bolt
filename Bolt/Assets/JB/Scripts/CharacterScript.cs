585f667201f20005c307f8b9a4de0052cc3053f0
Jaime Bews <bews.a.james@gmail.com>
2019-05-09 15:34:45 -0400

    Frisbee returning

--- Bolt/Assets/JB/Scripts/CharacterScript.cs
+++ Bolt/Assets/JB/Scripts/CharacterScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterScript : MonoBehaviour
 {
     private float m_speed = 1;
@@ -9,10 +10,16 @@ public class CharacterScript : MonoBehaviour
     public GameObject Frisbee;
     float yaw = 0;
     public GameObject CameraPivot;
+    public Transform returnTransform;
+    private Vector3 frisbeeLocalOffset;
+    bool returningFrisbee = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        frisbeeLocalOffset = Frisbee.transform.localPosition;
+        // initPosition.position += Frisbee.transform.localPosition;
+        //initPosition.localEulerAngles += Frisbee.transform.localEulerAngles;
     }
 
     // Update is called once per frame
@@ -26,18 +33,21 @@ public class CharacterScript : MonoBehaviour
         anim.SetFloat("Speed", verticalMovement);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            anim.SetBool("Attacking", true);
+            if (Frisbee.transform.parent != null) {
+                anim.SetBool("Attacking", true);
+            } else {
+                returningFrisbee = true;
+            }
+        }
+        if (returningFrisbee) {
+            ReturnFrisbee();
         }
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("Frisbee Throw")) {
             anim.SetBool("Attacking", false);
+           
         }
-            //this.transform.position += transform.forward * verticalMovement * m_speed;
-            //  this.transform.position += transform.right * horizontalMovement * m_speed;
-
-
              yaw += Input.GetAxis("Mouse X");
-            // pitch -= speedV * Input.GetAxis("Mouse Y"); //need to put this stuff on camera
-
+           
              CameraPivot.transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
              faceCamera();
         }
@@ -45,9 +55,27 @@ public class CharacterScript : MonoBehaviour
         transform.eulerAngles = new Vector3(0,Mathf.Lerp(transform.eulerAngles.x, yaw, 1), 0);
     }
     void Release() {
+        Frisbee.GetComponent<TrailRenderer>().emitting = true;
         Frisbee.transform.parent = null;
         Frisbee.GetComponent<Rigidbody>().isKinematic = false;
-        Frisbee.GetComponent<Rigidbody>().AddForce((this.transform.forward+(this.transform.up/3)) * 1000);
+        Frisbee.GetComponent<Rigidbody>().AddForce((this.transform.forward+(this.transform.up/3)) * 1500);
       
+    }
+    void ReturnFrisbee() {
+        Frisbee.GetComponent<Rigidbody>().useGravity = false;
+        Debug.Log("returnFrisbee");
+        float step = 10 * Time.deltaTime;//1 = speed
+        Frisbee.transform.position = Vector3.MoveTowards(Frisbee.transform.position, returnTransform.position, step);
+        Vector3 newDir = Vector3.RotateTowards(Frisbee.transform.forward, returnTransform.up, step, 0.0f);
+        Frisbee.transform.rotation = Quaternion.LookRotation(newDir);
+        if (Frisbee.transform.position == returnTransform.position) {
+            Debug.Log("Frisbee arrived");
+            returningFrisbee = false;
+            Frisbee.GetComponent<Rigidbody>().isKinematic = true;
+            Frisbee.GetComponent<Rigidbody>().useGravity = true;
+            Frisbee.transform.parent = returnTransform;
+            Frisbee.transform.localPosition = frisbeeLocalOffset;
+            Frisbee.GetComponent<TrailRenderer>().emitting = false;
+        }
     }
 }
---
+++ Bolt/Assets/JB/Scripts/FrisbeeScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrisbeeScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
