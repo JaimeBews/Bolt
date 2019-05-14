78d84661893ca0f53e63ff3e2cb03ce37319b807
Jaime Bews <bews.a.james@gmail.com>
2019-05-14 15:43:16 -0400

    army flock

---
+++ Bolt/Assets/JB/Scripts/ArmyScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ArmyScript : MonoBehaviour// actually I think this will be a parent class
+{
+
+    public bool         m_isLeader;
+    public int          m_numFollowers = 0;
+    // public int  m_team=0;//0 reserved for neutrals
+    // public int  type;// going to want an enum or maybe different classes per army type bows/guns swords seem to want to behave differently probably
+    public GameObject   m_prefab;
+    public float        m_speed=1;
+    private int         m_maxFollowers = 3;
+    private GameObject  m_leader=null;
+    private Transform   m_destination;
+    private Rigidbody   m_rb;
+    private Object[]    m_formations;
+    private int         m_unitID=0;
+    private GameObject  m_formation;
+    private int         m_currFormation=0;
+    private List<GameObject> m_units;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_isLeader) {
+            m_units = new List<GameObject>();
+            m_formations = Resources.LoadAll("Formations", typeof(GameObject));
+            m_formation = Instantiate(m_formations[m_currFormation], this.transform)as GameObject;
+            m_formation.transform.parent = this.transform;
+            m_formation.name = "Formation";
+            SpawnUnits(3);// just to spawn some in
+        }
+        m_rb = GetComponent<Rigidbody>();
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_leader!=null) {
+
+            this.transform.position = Vector3.MoveTowards(this.transform.position, m_destination.position, m_speed);// really basic movement
+        }else {
+            if (Input.GetKeyDown(KeyCode.Q)) {
+                ChangeFormations(true);
+            }
+            if (Input.GetKeyDown(KeyCode.E)) {
+                ChangeFormations(false);
+            }
+        }
+    }
+    void SetLeader(GameObject leader) {
+        m_leader = leader;
+    }
+    void SpawnUnits(int amount) {
+        for (int i = 0; i < amount; i++) {
+            GameObject unit = Instantiate( m_prefab, this.transform.position - this.transform.forward * (3+i), Quaternion.identity)as GameObject;
+            ArmyScript unitScript = unit.GetComponent<ArmyScript>();
+            unitScript.SetLeader(this.gameObject);
+            unitScript.m_unitID = this.m_numFollowers;
+            unitScript.m_destination = this.m_formation.transform.GetChild(unitScript.m_unitID).transform;
+            this.m_numFollowers++;
+            this.m_units.Add(unit);
+        }
+
+    }
+    void ResetDestination() {
+        foreach(GameObject unit in m_units) {
+            ArmyScript unitScript = unit.GetComponent<ArmyScript>();
+            unitScript.m_destination = this.m_formation.transform.GetChild(unitScript.m_unitID).transform;
+        }
+    }
+    void ChangeFormations(bool forwardChange) {
+        Destroy(m_formation);
+        m_formation = null;
+        int changeBy = 1;
+        if (!forwardChange)
+            changeBy = -1;
+        m_currFormation+= changeBy;
+        if (m_currFormation < 0)
+            m_currFormation = m_formations.Length-1;
+        if (m_currFormation > m_formations.Length - 1) {
+            m_currFormation = 0;
+        }
+        m_formation = Instantiate(m_formations[m_currFormation], this.transform) as GameObject;
+        m_formation.transform.parent = this.transform;
+        m_formation.name = "Formation";
+        ResetDestination();
+    }
+}
--- Bolt/Assets/JB/Scripts/CharacterScript.cs
+++ Bolt/Assets/JB/Scripts/CharacterScript.cs
@@ -12,20 +12,21 @@ public class CharacterScript : MonoBehaviour
     public GameObject CameraPivot;
     public Transform returnTransform;
     private Vector3 frisbeeLocalOffset;
+    private Vector3 frisbeeRot;
     bool returningFrisbee = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         frisbeeLocalOffset = Frisbee.transform.localPosition;
-        // initPosition.position += Frisbee.transform.localPosition;
-        //initPosition.localEulerAngles += Frisbee.transform.localEulerAngles;
+        frisbeeRot = Frisbee.transform.localEulerAngles;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       
         float verticalMovement = Input.GetAxis("Vertical");
         float horizontalMovement = Input.GetAxis("Horizontal");
         anim.SetBool("Moving", verticalMovement != 0);
@@ -51,10 +52,10 @@ public class CharacterScript : MonoBehaviour
              CameraPivot.transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
              faceCamera();
         }
-    void faceCamera() {
+    void faceCamera() {//mouse controlled
         transform.eulerAngles = new Vector3(0,Mathf.Lerp(transform.eulerAngles.x, yaw, 1), 0);
     }
-    void Release() {
+    void Release() {//called by frisbeetoss animation
         Frisbee.GetComponent<TrailRenderer>().emitting = true;
         Frisbee.transform.parent = null;
         Frisbee.GetComponent<Rigidbody>().isKinematic = false;
@@ -64,17 +65,18 @@ public class CharacterScript : MonoBehaviour
     void ReturnFrisbee() {
         Frisbee.GetComponent<Rigidbody>().useGravity = false;
         Debug.Log("returnFrisbee");
-        float step = 10 * Time.deltaTime;//1 = speed
+        float step = 40 * Time.deltaTime;//40 = speed = unexposed/not ideal
         Frisbee.transform.position = Vector3.MoveTowards(Frisbee.transform.position, returnTransform.position, step);
         Vector3 newDir = Vector3.RotateTowards(Frisbee.transform.forward, returnTransform.up, step, 0.0f);
         Frisbee.transform.rotation = Quaternion.LookRotation(newDir);
-        if (Frisbee.transform.position == returnTransform.position) {
+        if (Frisbee.transform.position == returnTransform.position) {//TODO: No check for failure to return could time it out with teleported return
             Debug.Log("Frisbee arrived");
             returningFrisbee = false;
             Frisbee.GetComponent<Rigidbody>().isKinematic = true;
             Frisbee.GetComponent<Rigidbody>().useGravity = true;
             Frisbee.transform.parent = returnTransform;
             Frisbee.transform.localPosition = frisbeeLocalOffset;
+            Frisbee.transform.localEulerAngles = frisbeeRot;
             Frisbee.GetComponent<TrailRenderer>().emitting = false;
         }
     }
